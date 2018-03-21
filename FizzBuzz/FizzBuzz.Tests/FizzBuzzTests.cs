30a88b90fd8f1af5adaf519cb3f2584866a378ca
Ben Scott <bwscott@gmail.com>
2018-03-21 19:33:54 +0000

    Updating Part2

--- FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -21,6 +21,35 @@ public class FizzBuzzTests
     [Test]
     public void FizzBuzzer_Can_Handle_Fizz()
     {
+        var startValue = 6;
+        var endValue = 7;
+
+        var fizzBuzzer = new FizzBuzzer(startValue, endValue);
+        var result = fizzBuzzer.FizzBuzzify();
+
+        var expectedResult = "fizz 7";
+
+        Assert.IsNotNull(result);        
+        Assert.AreEqual(expectedResult, result);
+    }
+    [Test]
+    public void FizzBuzzer_Can_Handle_Buzz()
+    {
+        var startValue = 5;
+        var endValue = 6;
+
+        var fizzBuzzer = new FizzBuzzer(startValue, endValue);
+        var result = fizzBuzzer.FizzBuzzify();
+
+        var expectedResult = "buzz fizz";
+
+        Assert.IsNotNull(result);        
+        Assert.AreEqual(expectedResult, result);
+    }
+
+    [Test]
+    public void FizzBuzzer_Can_Handle_Lucky()
+    {
         var startValue = 1;
         var endValue = 3;
 
@@ -29,38 +58,37 @@ public class FizzBuzzTests
 
         var expectedResult = "1 2 lucky";
 
-        Assert.IsNotNull(result);        
+        Assert.IsNotNull(result);
         Assert.AreEqual(expectedResult, result);
     }
+
     [Test]
-    public void FizzBuzzer_Can_Handle_Buzz()
+    public void FizzBuzzer_Can_Handle_FizzBuzz()
     {
-        var startValue = 1;
-        var endValue = 5;
+        var startValue = 14;
+        var endValue = 16;
 
         var fizzBuzzer = new FizzBuzzer(startValue, endValue);
         var result = fizzBuzzer.FizzBuzzify();
 
-        var expectedResult = "1 2 lucky 4 buzz";
+        var expectedResult = "14 fizzbuzz 16";
 
-        Assert.IsNotNull(result);        
+        Assert.IsNotNull(result);
         Assert.AreEqual(expectedResult, result);
     }
 
     [Test]
-    public void FizzBuzzer_Can_Handle_Lucky_And_FizzBuzz()
+    public void FizzBuzzer_Can_Handle_All_Variations()
     {
         var startValue = 1;
         var endValue = 20;
 
         var fizzBuzzer = new FizzBuzzer(startValue, endValue);
         var result = fizzBuzzer.FizzBuzzify();
-
+        
         var expectedResult = "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz";
 
         Assert.IsNotNull(result);
         Assert.AreEqual(expectedResult, result);
     }
-
-
 }
