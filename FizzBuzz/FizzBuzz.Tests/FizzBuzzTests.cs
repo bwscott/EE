67eb4bd625864ac84a490f3a75c532ab20addeb4
Ben Scott <bwscott@gmail.com>
2018-03-21 19:07:23 +0000

    Part 3 commit

--- FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -12,7 +12,13 @@ public class FizzBuzzTests
         var fizzBuzzer = new FizzBuzzer(startValue, endValue);
         var result = fizzBuzzer.FizzBuzzify();
 
-        var expectedResult = "1 2";
+        var expectedIntCount = 2;
+        var expectedFizzCount = 0;
+        var expectedBuzzCount = 0;
+        var expectedFizzBuzzCount = 0;
+        var expectedLuckyCount = 0;
+
+        var expectedResult = $"1 2 fizz:{expectedFizzCount} buzz: {expectedBuzzCount} fizzbuzz: {expectedFizzBuzzCount} lucky: {expectedLuckyCount} integer: {expectedIntCount}";
 
         Assert.IsNotNull(result);        
         Assert.AreEqual(expectedResult, result);        
@@ -27,28 +33,83 @@ public class FizzBuzzTests
         var fizzBuzzer = new FizzBuzzer(startValue, endValue);
         var result = fizzBuzzer.FizzBuzzify();
 
-        var expectedResult = "1 2 lucky";
+        var expectedIntCount = 2;
+        var expectedFizzCount = 0;
+        var expectedBuzzCount = 0;
+        var expectedFizzBuzzCount = 0;
+        var expectedLuckyCount = 1;
+
+        var expectedResult = $"1 2 lucky fizz:{expectedFizzCount} buzz: {expectedBuzzCount} fizzbuzz: {expectedFizzBuzzCount} lucky: {expectedLuckyCount} integer: {expectedIntCount}";
 
         Assert.IsNotNull(result);        
         Assert.AreEqual(expectedResult, result);
     }
+
     [Test]
     public void FizzBuzzer_Can_Handle_Buzz()
     {
-        var startValue = 1;
-        var endValue = 5;
+        var startValue = 5;
+        var endValue = 6;
 
         var fizzBuzzer = new FizzBuzzer(startValue, endValue);
         var result = fizzBuzzer.FizzBuzzify();
 
-        var expectedResult = "1 2 lucky 4 buzz";
+        var expectedIntCount = 0;
+        var expectedFizzCount = 1;
+        var expectedBuzzCount = 1;
+        var expectedFizzBuzzCount = 0;
+        var expectedLuckyCount = 0;
+
+        var expectedResult = $"buzz fizz fizz:{expectedFizzCount} buzz: {expectedBuzzCount} fizzbuzz: {expectedFizzBuzzCount} lucky: {expectedLuckyCount} integer: {expectedIntCount}";
 
         Assert.IsNotNull(result);        
         Assert.AreEqual(expectedResult, result);
     }
 
     [Test]
-    public void FizzBuzzer_Can_Handle_Lucky_And_FizzBuzz()
+    public void FizzBuzzer_Can_Handle_Lucky()
+    {
+        var startValue = 1;
+        var endValue = 3;
+
+        var fizzBuzzer = new FizzBuzzer(startValue, endValue);
+        var result = fizzBuzzer.FizzBuzzify();
+
+        var expectedIntCount = 2;
+        var expectedFizzCount = 0;
+        var expectedBuzzCount = 0;
+        var expectedFizzBuzzCount = 0;
+        var expectedLuckyCount = 1;
+
+        var expectedResult = $"1 2 lucky fizz:{expectedFizzCount} buzz: {expectedBuzzCount} fizzbuzz: {expectedFizzBuzzCount} lucky: {expectedLuckyCount} integer: {expectedIntCount}";
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(expectedResult, result);
+    }
+
+    [Test]
+    public void FizzBuzzer_Can_Handle_FizzBuzz()
+    {
+        var startValue = 14;
+        var endValue = 16;
+
+        var fizzBuzzer = new FizzBuzzer(startValue, endValue);
+        var result = fizzBuzzer.FizzBuzzify();
+
+        var expectedIntCount = 2;
+        var expectedFizzCount = 0;
+        var expectedBuzzCount = 0;
+        var expectedFizzBuzzCount = 1;
+        var expectedLuckyCount = 0;
+
+        var expectedResult = $"14 fizzbuzz 16 fizz:{expectedFizzCount} buzz: {expectedBuzzCount} fizzbuzz: {expectedFizzBuzzCount} lucky: {expectedLuckyCount} integer: {expectedIntCount}";
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(expectedResult, result);
+    }
+
+    [Test]
+    public void FizzBuzzer_Can_Handle_All_Variations()
     {
         var startValue = 1;
         var endValue = 20;
@@ -56,11 +117,15 @@ public class FizzBuzzTests
         var fizzBuzzer = new FizzBuzzer(startValue, endValue);
         var result = fizzBuzzer.FizzBuzzify();
 
-        var expectedResult = "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz";
+        var expectedIntCount = 10;
+        var expectedFizzCount = 4;
+        var expectedBuzzCount = 3;
+        var expectedFizzBuzzCount = 1;
+        var expectedLuckyCount = 2;
+
+        var expectedResult = $"1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz fizz:{expectedFizzCount} buzz: {expectedBuzzCount} fizzbuzz: {expectedFizzBuzzCount} lucky: {expectedLuckyCount} integer: {expectedIntCount}";
 
         Assert.IsNotNull(result);
         Assert.AreEqual(expectedResult, result);
     }
-
-
 }
--- FizzBuzz/FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzz/FizzBuzzer.cs
@@ -9,6 +9,12 @@
     private int StartValue;
     private int EndValue;
 
+    private int FizzCount = 0;
+    private int BuzzCount = 0;
+    private int FizzBuzzCount = 0;
+    private int LuckyCount = 0;
+    private int IntCount = 0;
+
     public string FizzBuzzify()
     {
         string result = string.Empty;
@@ -17,6 +23,8 @@
         {
             result += ProcessNumber(i);
         }
+
+        result += $" fizz:{FizzCount} buzz: {BuzzCount} fizzbuzz: {FizzBuzzCount} lucky: {LuckyCount} integer: {IntCount}";
         return result.Trim();
     }
 
@@ -43,24 +51,31 @@
     private string ProcessNumber(int i)
     {
         if (ContainsThree(i))
+        {
+            LuckyCount++;
             return string.Concat(" ", "lucky");
+        }
 
         if (FizzBuzzable(i))
         {
+            FizzBuzzCount++;
             return string.Concat(" " + "fizzbuzz");
         }
         else
         {
             if (Fizzable(i))
             {
+                FizzCount++;
                 return string.Concat(" ", "fizz");
             }
 
             if (Buzzable(i))
             {
+                BuzzCount++;
                 return string.Concat(" ", "buzz");
             }
 
+            IntCount++;
             return string.Concat(" ", i);
         }
     }
